cbe17f3ff42e4ed8553630190fc40722044a2863
Andre Luiz Goncalves Teixeira <andre.teixeira@compasso.com.br>
2022-01-12 14:47:55 -0300

    feat: Test dependencies added and server divided in app/server

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,3 @@
-import "reflect-metadata";
-
-import express from "express";
-
-import swaggerUi from "swagger-ui-express";
-
-import {routes} from "./routes/index"; 
-
-import swaggerDocs from "./swagger.json";
-
-import "./infra/database/postgres";
-
-const app = express();
-
-app.use(express.json());
-
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-
-app.use(routes);
+import { app } from "./app";
 
 app.listen(3000);
